0c8f44083cb55c9754080c5925788c131b48f3b4
Omar Bah <omarbah2007@gmail.com>
2025-12-21 00:50:21 -0500

    flowing but ugly walking animation
    animation is ugly and looks weird but it flows now

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -11,10 +11,13 @@ public class movement : MonoBehaviour
     private Vector3 movingV3;
     private Rigidbody rb;
     public int moveSpeed;
+
+    private Animator animator;
     void Start()
     {
         playerTransform = gameObject.transform;
         rb = gameObject.GetComponent<Rigidbody>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,6 +25,14 @@ public class movement : MonoBehaviour
     {
         movingV2 = move.action.ReadValue<Vector2>();
         movingV3 = new Vector3(movingV2.x * moveSpeed * Time.deltaTime,0,movingV2.y* moveSpeed * Time.deltaTime);
+        if(movingV3.Equals(new Vector3(0,0,0)))
+        {
+            animator.SetBool("isWalking",false);
+        }
+        else
+        {
+            animator.SetBool("isWalking",true);
+        }
     }
     void FixedUpdate()
     {
